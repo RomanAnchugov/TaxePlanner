dfc334424fe9101bb3e40ae6b1af0881f3ad8123
RomanAnchugov <romagn@mail.ru>
2018-01-28 00:27:43 +0300

    Description dialog fragment
    -realised full functioonal of this dialog
    -sharing data with fragment of creation
    -saving data in order item
    -comments)
    -also some if's for validation
    -created todo for the next task

--- app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/CreateNewOrderFragment.java
+++ app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/CreateNewOrderFragment.java
@@ -1,6 +1,8 @@
 package ru.taxiplanner.romananchugov.taxiplanner;
 
+import android.app.Activity;
 import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -22,6 +24,7 @@ import ru.taxiplanner.romananchugov.taxiplanner.dialogs.TimePickerDialogFragment
 public class CreateNewOrderFragment extends DialogFragment implements View.OnClickListener {
 
     private static final String TAG = "CreateNewOrderFragment";
+    private static final int REQUEST_CODE_FOR_DESCRIPTION = 1;
 
     private LinearLayout orderDateContainer;
     private TextView orderDateStatus;
@@ -31,6 +34,9 @@ public class CreateNewOrderFragment extends DialogFragment implements View.OnCli
 
     private LinearLayout orderDescriptionContainer;
     private TextView orderDescriptionStatus;
+
+    private LinearLayout orderNumberOfSeatsContainer;
+    private TextView orderNumberOfSeatsStatus;
 
     private Button submitNewOrderButton;
 
@@ -54,6 +60,10 @@ public class CreateNewOrderFragment extends DialogFragment implements View.OnCli
         orderDescriptionStatus = (TextView) v.findViewById(R.id.set_order_description_status);
         orderDescriptionContainer.setOnClickListener(this);
 
+        orderNumberOfSeatsContainer = (LinearLayout) v.findViewById(R.id.set_number_of_seats_container);
+        orderNumberOfSeatsStatus = (TextView) v.findViewById(R.id.set_number_of_seats_status);
+        orderNumberOfSeatsContainer.setOnClickListener(this);
+
         submitNewOrderButton = (Button) v.findViewById(R.id.submit_new_order_button);
         submitNewOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,10 +84,41 @@ public class CreateNewOrderFragment extends DialogFragment implements View.OnCli
                 new TimePickerDialogFragment(orderItem, orderTimeStatus).show(getFragmentManager(), "time picker");
                 break;
             case R.id.set_order_description_container:
-                new DescriptionDialogFragment().show(getFragmentManager(), "description dialog");
+                //SHARING AN EXISTING DESCRIPTION WITH DIALOG
+                Bundle args = new Bundle();
+                args.putString(DescriptionDialogFragment.EXTRA_DESCRIPTION_TAG, orderDescriptionStatus.getText().toString());
+                DescriptionDialogFragment descriptionDialogFragment = new DescriptionDialogFragment();
+                descriptionDialogFragment.setArguments(args);
+                descriptionDialogFragment.setTargetFragment(this, REQUEST_CODE_FOR_DESCRIPTION);
+                descriptionDialogFragment.show(getFragmentManager(), "description dialog");
+                break;
+            case R.id.set_number_of_seats_container:
+                /*TODO: create dialog fragment fragment for choosing number of seats, and some additions:
+                        -create new fields in database for maxValueOfSeats and currentValueOfSeats
+                        -create new .xml and other staff
+                        -add sharing data between dialog and fragment like in previous case
+                 */
                 break;
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        switch (requestCode){
+            case REQUEST_CODE_FOR_DESCRIPTION://WORKING WITH NEW DESCRIPTION
+                if(resultCode == Activity.RESULT_OK){
+                    Bundle bundle = data.getExtras();
+                    String mDescription = bundle.getString(DescriptionDialogFragment.EXTRA_DESCRIPTION_TAG, "fuck");
+                    orderItem.setDescription(mDescription);
+                    if(!mDescription.equals("")) {
+                        orderDescriptionStatus.setText(mDescription);
+                    }else{
+                        orderDescriptionStatus.setText(getResources().getString(R.string.not_written));
+                    }
+                }
+                break;
+        }
+    }
 }
--- app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/dialogs/DescriptionDialogFragment.java
+++ app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/dialogs/DescriptionDialogFragment.java
@@ -1,11 +1,15 @@
 package ru.taxiplanner.romananchugov.taxiplanner.dialogs;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import ru.taxiplanner.romananchugov.taxiplanner.R;
 
@@ -14,25 +18,46 @@ import ru.taxiplanner.romananchugov.taxiplanner.R;
  */
 
 public class DescriptionDialogFragment extends DialogFragment {
+    public static final String EXTRA_DESCRIPTION_TAG = "Description";
+
+    private EditText descriptionEditText;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.new_order_description, null));
+        View v = inflater.inflate(R.layout.new_order_description, null);
+        Bundle bundle = getArguments();
+
+        //GET EXISTING DESCRIPTION
+        descriptionEditText = (EditText) v.findViewById(R.id.new_order_description_edit_text);
+        if(!bundle.getString(EXTRA_DESCRIPTION_TAG).equals("not written")) {
+            descriptionEditText.setText(bundle.getString(EXTRA_DESCRIPTION_TAG));
+        }else{
+            descriptionEditText.setText("");
+        }
+
+        builder.setView(v);
         builder.setTitle("Description");
+        //OK CLICK
         builder.setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                //TODO: sharing data with "CreateNewOrderFragment"
+                //SETTING NEW DESCRIPTION
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_DESCRIPTION_TAG, descriptionEditText.getText().toString());
+                getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
                 dismiss();
             }
-        });
+        });//OK CLICK
+
+        //CANCEL CLICK
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dismiss();
             }
-        });
+        });//CANCEL CLICK
 
         return builder.create();
     }
