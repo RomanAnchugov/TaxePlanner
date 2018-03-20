cb0b62823247e6e513540ba167bab1da633b83b1
RomanAnchugov <romagn@mail.ru>
2018-03-20 23:18:37 +0300

    Implemented full only-phone functional
     - remade starter fragment
     - remade registration fragment(didn't test)

--- app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/fragments/RegistrationFragment.java
+++ app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/fragments/RegistrationFragment.java
@@ -1,30 +1,15 @@
 package ru.taxiplanner.romananchugov.taxiplanner.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.telephony.PhoneNumberUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import ru.taxiplanner.romananchugov.taxiplanner.MainActivity;
 import ru.taxiplanner.romananchugov.taxiplanner.R;
@@ -34,7 +19,8 @@ import ru.taxiplanner.romananchugov.taxiplanner.service.UserItem;
  * Created by romananchugov on 16.02.2018.
  */
 
-public class RegistrationFragment extends Fragment implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+@SuppressLint("ValidFragment")
+public class RegistrationFragment extends Fragment implements View.OnClickListener{
 
     private final String TAG = "RegistrationFragment";
 
@@ -42,38 +28,22 @@ public class RegistrationFragment extends Fragment implements View.OnClickListen
 
     private EditText registrationName;
     private EditText registrationSurname;
-    private EditText registrationPhoneNumber;
-    private EditText registrationEmail;
-    private EditText registrationPassword;
-    private RadioGroup verificationRadioGroup;
-    private RadioButton registrationVerifyPhone;
-    private RadioButton registrationVerifyEmail;
-    private Button registrationSubmitButton;
+
+    private String phoneNumber;
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
+    public RegistrationFragment(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
     @Nullable
     @Override
-
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.registration_fragment, container, false);
 
         registrationName = v.findViewById(R.id.registration_name_edit_text);
         registrationSurname = v.findViewById(R.id.registration_surname_edit_text);
-        registrationPhoneNumber = v.findViewById(R.id.registration_phone_number_edit_text);
-        registrationEmail = v.findViewById(R.id.registration_email_edit_text);
-        registrationPassword = v.findViewById(R.id.registration_password_edit_text);
-
-        verificationRadioGroup = v.findViewById(R.id.registration_radio_group);
-        verificationRadioGroup.setOnCheckedChangeListener(this);
-
-        registrationVerifyPhone = v.findViewById(R.id.registration_verify_number_radio_button);
-        registrationVerifyPhone.setChecked(true);
-        registrationVerifyEmail = v.findViewById(R.id.registration_verify_email_radio_button);
-
-        registrationSubmitButton = v.findViewById(R.id.registration_submit_button);
-
-        registrationSubmitButton.setOnClickListener(this);
 
         return v;
     }
@@ -82,118 +52,14 @@ public class RegistrationFragment extends Fragment implements View.OnClickListen
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.registration_submit_button:
-                if(isValid()) {
-                    Log.i(TAG, "onClick: registration submit button");
-                    if(registrationVerifyEmail.isChecked()){
-                        createAccountEmail(registrationEmail.getText().toString(), registrationPassword.getText().toString());
-                    }else if(registrationVerifyPhone.isChecked()){
-                        MainActivity.goToFragment(
-                                new PhoneVerificationFragment(
+                MainActivity.goToFragment(
+                        new PhoneVerificationFragment(
                                 new UserItem(
                                         registrationName.getText().toString(),
                                         registrationSurname.getText().toString(),
-                                        registrationPhoneNumber.getText().toString()
-                                        )), getActivity(), true);
-                        //createAccountPhone(registrationEmail.getText().toString(), registrationPassword.getText().toString());
-                    }
-                }
+                                        phoneNumber
+                                )), getActivity(), true);
         }
     }
 
-    public boolean isValid(){
-        String email = registrationEmail.getText().toString();
-        Pattern p = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher(email);
-
-        if((!registrationEmail.getText().toString().equals("") || !registrationEmail.isEnabled())
-                && !registrationName.getText().toString().equals("")
-                && !registrationSurname.getText().toString().equals("")
-                && !registrationPhoneNumber.getText().toString().equals("")
-                && (!registrationPassword.getText().toString().equals("") || !registrationPassword.isEnabled())){
-            if((registrationPassword.getText().toString().length() >= 6 || !registrationPassword.isEnabled())){
-                if((m.find() || !registrationEmail.isEnabled())){
-                    if(isPhoneNumberValid()) {
-                        return true;
-                    }
-                }else{
-                    Snackbar.make(getView(), "Incorrect email", Snackbar.LENGTH_SHORT).show();
-                }
-            }else{
-                Snackbar.make(getView(), "Password should have size >= 6", Snackbar.LENGTH_SHORT).show();
-            }
-        }else{
-            Snackbar.make(getView(), R.string.fill_all_gaps, Snackbar.LENGTH_SHORT).show();
-        }
-        return false;
-    }
-
-    private void createAccountEmail(String userEmail, String userPassword){
-        mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                            final String name = registrationName.getText().toString();
-                            final String surname = registrationSurname.getText().toString();
-                            final String phoneNumber = registrationPhoneNumber.getText().toString();
-
-                            //sending verification email
-                            user.sendEmailVerification().addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        UserItem userItem = new UserItem(name, surname, phoneNumber);
-                                        FirebaseDatabase.getInstance().getReference("users/" + mAuth.getUid()).setValue(userItem);
-                                        Log.i(TAG, "Send verification email: successful");
-                                        Toast.makeText(getActivity(), "We will send you verification email, check it", Toast.LENGTH_LONG).show();
-
-                                        getFragmentManager().popBackStackImmediate();
-                                    }else{
-                                        Log.i(TAG, "Send verification email: failed");
-                                        Toast.makeText(getActivity(), "Some problem, try again", Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        } else {
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-    private boolean isPhoneNumberValid(){
-        String number = registrationPhoneNumber.getText().toString();
-        if(PhoneNumberUtils.isGlobalPhoneNumber(number)
-                && number.length() == 12
-                && number.charAt(0) == '+'
-                && number.charAt(1) == '7'){
-            return true;
-        }else{
-            Snackbar.make(getView(), "Invalid phone number", Snackbar.LENGTH_SHORT).show();
-            return false;
-        }
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int id) {
-        RadioButton radioButton = radioGroup.findViewById(id);
-        if(radioButton.isChecked()){
-            switch (id){
-                case R.id.registration_verify_number_radio_button:
-                    registrationEmail.setEnabled(false);
-                    registrationPassword.setEnabled(false);
-                    break;
-                case R.id.registration_verify_email_radio_button:
-                    registrationEmail.setEnabled(true);
-                    registrationPassword.setEnabled(true);
-            }
-        }
-
-
-    }
 }
--- app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/fragments/StarterFragment.java
+++ app/src/main/java/ru/taxiplanner/romananchugov/taxiplanner/fragments/StarterFragment.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.text.Editable;
@@ -18,9 +17,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,8 +32,6 @@ import ru.taxiplanner.romananchugov.taxiplanner.MainActivity;
 import ru.taxiplanner.romananchugov.taxiplanner.R;
 import ru.taxiplanner.romananchugov.taxiplanner.service.UserItem;
 
-import static ru.taxiplanner.romananchugov.taxiplanner.MainActivity.goToFragment;
-
 /**
  * Created by romananchugov on 28.12.2017.
  */
@@ -47,10 +41,7 @@ public class StarterFragment extends Fragment {
 
     private FirebaseAuth mAuth;
 
-//    private EditText userEmail;
-//    private EditText userPassword;
     private EditText userPhoneNumber;
-    private Button registrationButton;
     private Button loginButton;
     private ProgressBar progressBar;
 
@@ -78,30 +69,13 @@ public class StarterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.starter_fragment, container, false);
-//        userEmail = v.findViewById(R.id.user_email_edit_text);
-//        userPassword = v.findViewById(R.id.user_password_edit_text);
         userPhoneNumber = v.findViewById(R.id.sig_in_phone_number_edit_text);
-        registrationButton = v.findViewById(R.id.user_registration_button);
         progressBar = v.findViewById(R.id.starter_fragment_progress_bar);
-        registrationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToFragment(new RegistrationFragment(), getActivity(), true);
-            }
-        });
         loginButton = v.findViewById(R.id.user_login_button);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //email login
-//                if(isValidInput()) {
-//                    signIn(userEmail.getText().toString(), userPassword.getText().toString());
-//                    progressBar.setVisibility(View.VISIBLE);
-//                }else{
-//                    Snackbar.make(getView(), "Invalid input", Snackbar.LENGTH_SHORT).show();
-//                }
 
                 //phone number login
                 if(isValidInput()) {
@@ -137,35 +111,6 @@ public class StarterFragment extends Fragment {
         return v;
     }
 
-    //email login
-    private void signIn(String userEmail, String userPassword){
-        mAuth.signInWithEmailAndPassword(userEmail, userPassword)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressBar.setVisibility(View.GONE);
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success -> checking for verification");
-
-                            //checking for verify email
-                            if(!mAuth.getCurrentUser().isEmailVerified()){
-                                Log.d(TAG, "not verified user");
-                                Toast.makeText(getActivity(), "Please, verify your email", Toast.LENGTH_LONG).show();
-                            }else{
-                                Log.i(TAG, "verified user -> go to main fragment");
-                                goToSearchFragment();
-                            }
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(getActivity(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
     public void goToSearchFragment(){
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -194,6 +139,7 @@ public class StarterFragment extends Fragment {
                 progressBar.setVisibility(View.GONE);
                 GenericTypeIndicator<HashMap<String, UserItem>> generic = new GenericTypeIndicator<HashMap<String, UserItem>>(){};//type indicator
 
+                Log.i(TAG, "onDataChange: " + dataSnapshot.getValue(generic));
                 HashMap<String, UserItem> list = dataSnapshot.getValue(generic);
                 for(HashMap.Entry<String, UserItem> entry: list.entrySet()){
                     UserItem userItem = entry.getValue();
@@ -205,7 +151,12 @@ public class StarterFragment extends Fragment {
                     }
                 }
                 Log.i(TAG, "onDataChange: we didn't found number in database");
-                Snackbar.make(getView(), "You have to sign up", Snackbar.LENGTH_LONG).show();
+                MainActivity.goToFragment(
+                        new RegistrationFragment(userPhoneNumber.getText().toString()),
+                        getActivity(),
+                        true
+                );
+                Toast.makeText(getActivity(), "You have to sign up", Toast.LENGTH_LONG).show();
             }
 
             @Override
